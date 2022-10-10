c62f996abbf6c8a32ae1d0548cda498415bad53d
Thomas Calvente <thomycalvente@gmail.com>
2022-10-10 19:35:30 -0300

    Back + extra

---
+++ src/main/java/com/egg/AppRECC/controladores/PosteoControlador.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.egg.AppRECC.controladores;
+
+import com.egg.AppRECC.entidades.Posteo;
+import com.egg.AppRECC.excepciones.MiException;
+import com.egg.AppRECC.servicios.PosteoServicio;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ *
+ * @author PC
+ */
+@Controller
+@RequestMapping("posteos")
+public class PosteoControlador {
+    
+    @Autowired
+    private PosteoServicio posteoServicio;
+    
+    @GetMapping("/publicar")
+    public String actividades() {
+        
+        return "posteos_form.html";
+    }
+    
+    @GetMapping("/detalle/{id}")
+    public String detalle(@PathVariable Long id, ModelMap modelo) {
+
+        Optional<Posteo> posteos = posteoServicio.findById(id);
+        modelo.addAttribute("posteos", posteos.get());
+        return "detail.html";
+    }
+    
+}
