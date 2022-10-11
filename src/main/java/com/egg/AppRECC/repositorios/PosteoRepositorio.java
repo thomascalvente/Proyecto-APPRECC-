4a4dd8fe397838bcb17e2610af97a24fa827123c
Gustavo Osvaldo Torti <guztorti@gmail.com>
2022-10-10 22:18:09 -0300

    sprint 1

--- src/main/java/com/egg/AppRECC/repositorios/PosteoRepositorio.java
+++ src/main/java/com/egg/AppRECC/repositorios/PosteoRepositorio.java
@@ -6,7 +6,10 @@
 package com.egg.AppRECC.repositorios;
 
 import com.egg.AppRECC.entidades.Posteo;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,5 +19,11 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PosteoRepositorio extends JpaRepository<Posteo, Long>{
+     
+    @Query("SELECT n FROM Posteo n WHERE n.titulo = :titulo")
+    public Posteo buscarPorTitulo(@Param("titulo") String titulo);
     
+    /* @Query("SELECT n FROM Posteo n WHERE n.borrado!=true ORDER BY n.fecha desc")
+    public List<Posteo> listarposteos();
+     */
 }
