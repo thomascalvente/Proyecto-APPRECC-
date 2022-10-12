74384d5d5674149e02ba0048b5fcf2d4ace6cd92
Thomas Calvente <thomycalvente@gmail.com>
2022-10-12 20:39:40 -0300

    Actualizacion

--- src/main/java/com/egg/AppRECC/entidades/Posteo.java
+++ src/main/java/com/egg/AppRECC/entidades/Posteo.java
@@ -6,14 +6,12 @@
 package com.egg.AppRECC.entidades;
 
 import java.time.LocalDate;
-import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import lombok.Data;
 
 /**
  *
@@ -21,6 +19,7 @@ import javax.persistence.TemporalType;
  */
 
 @Entity
+@Data
 public class Posteo {
     
     @Id
@@ -30,53 +29,12 @@ public class Posteo {
     private String titulo;
     private String cuerpo;
     
+    private boolean borrado;
+    
     @Column(columnDefinition = "MEDIUMTEXT")
     private String imagen;
     
     
     private LocalDate fecha;
-
-    public Posteo() {
-    }  
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getCuerpo() {
-        return cuerpo;
-    }
-
-    public void setCuerpo(String cuerpo) {
-        this.cuerpo = cuerpo;
-    }
-
-    public String getImagen() {
-        return imagen;
-    }
-
-    public void setImagen(String imagen) {
-        this.imagen = imagen;
-    }
-
-    public LocalDate getFecha() {
-        return fecha;
-    }
-
-    public void setFecha(LocalDate fecha) {
-        this.fecha = fecha;
-    }
     
 }
