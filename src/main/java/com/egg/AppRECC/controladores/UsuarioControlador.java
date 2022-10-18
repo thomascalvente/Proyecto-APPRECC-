3182f6730a86b79502f0ca7deb869a8a8e89126d
Marina Torres <torresmarina983@gmail.com>
2022-10-17 21:16:57 -0300

    commit (lunes 17 de entrega)

--- src/main/java/com/egg/AppRECC/controladores/UsuarioControlador.java
+++ src/main/java/com/egg/AppRECC/controladores/UsuarioControlador.java
@@ -1,7 +1,11 @@
 package com.egg.AppRECC.controladores;
 
+import com.egg.AppRECC.entidades.Posteo;
+import com.egg.AppRECC.entidades.Usuario;
 import com.egg.AppRECC.excepciones.MiException;
 import com.egg.AppRECC.servicios.UsuarioServicio;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -9,29 +13,47 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+ 
 @Controller
 @RequestMapping("/usuario")
 public class UsuarioControlador {
-    
+
     @Autowired
     private UsuarioServicio usuarioServicio;
-    
+
     @GetMapping("/registrar")
     public String registrar(){
         return "register.html";
     }
     
+    @GetMapping("/listar")
+    public String listar(ModelMap modelo){
+        List <Usuario> usuario = usuarioServicio.listarUsuarios();
+        modelo.addAttribute("usuarios", usuario);
+        return "listarUsuarios.html";
+    }
+
     @PostMapping("/registro")
     public String registro(@RequestParam("nombre") String nombre, @RequestParam("email") String email,
+<<<<<<< Updated upstream
             @RequestParam("password") String password, @RequestParam("password2") String password2, ModelMap modelo){
+=======
+            @RequestParam("password") String password,@RequestParam("password2") String password2, ModelMap modelo){
+ 
+>>>>>>> Stashed changes
         try {
             usuarioServicio.crearUsuario(nombre, email, password, password2);
             modelo.put("exito", "El usuario se creo correctamente");
             return "redirect:/";
         } catch (MiException e) {
             modelo.put("error", e.getMessage());
+<<<<<<< Updated upstream
             return "register.html";
+=======
+            return "user_form";
+>>>>>>> Stashed changes
         }
     }
+    
+    
 }
